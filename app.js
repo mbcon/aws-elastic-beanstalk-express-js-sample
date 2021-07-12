a2710872c2ebdf7b6d786b4c92015e5d0d69a614
mbcon <49731304+mbcon@users.noreply.github.com>
2021-07-12 14:49:34 +0200

    Underline

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello <b>Amazon</b>!'));
+app.get('/', (req, res) => res.send('Hello <b><u>Amazon</u></b>!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
